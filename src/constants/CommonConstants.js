95278db9ef03cb1101a8f75f771c801985c34ff5
Rohit Kumar Bisht <rk790000@gmail.com>
2024-04-21 20:06:37 +0530

    Create CommonConstants.js

---
+++ src/constants/CommonConstants.js
@@ -0,0 +1,10 @@
+export const OVERALL_PERFORMANCE = "Overall Performance"
+export const LEADER_BOARD = "Leaderboard"
+export const PERCENTAGE_LABEL = "Percentage";
+export const RANK_LABEL = "Rank";
+export const NAME_LABEL = "Name";
+export const SKILL_LABEL = "Skill";
+export const TOTAL_TEST_TIME_LABEL = "Total Test Time";
+export const NUMBER_OF_QUESTIONS_LABEL = "No. of Questions";
+export const TIME_SPENT_LABEL = "Time Spent";
+export const AUTH_ERROR = "AuthToken not found! Please login.";
