61a5876ab31fb59643a9b83b2c44a7c43f986dcd
Rohit Kumar Bisht <rk790000@gmail.com>
2024-04-21 20:07:26 +0530

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    apiUrl: process.env.REACT_APP_API_BASE_URL, 
+    overallAssessment: process.env.REACT_APP_OVERALL_ASSESSMENT,
+    individualAssessment: process.env.REACT_APP_INDIVIDUAL_ASSESSMENT,
+    teacherUrl: process.env.REACT_APP_TEACHER_URL,
+    reportsUrl: process.env.REACT_APP_REPORTS_URL
+  };
+  
+export default config;
